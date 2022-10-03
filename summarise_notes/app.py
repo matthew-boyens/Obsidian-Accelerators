885c14350b5bc9284b90d62f463781f026c7031c
Boyens, Matthew <mboyens@deloitte.com.au>
2022-10-03 16:08:08 +1100

    Number of revisions to convert the summarise function to a class, whilst making it generic to prompt

--- summarise_notes/app.py
+++ summarise_notes/app.py
@@ -47,77 +47,228 @@ MAX_INPUT_CHARS
 
 
 # %%
-def gpt_summary_on_podcast(prompts,filepath):
-    """Takes Prompts in format [["Prompt","Model"]] and applies them to the file found at filename """
+class Document:
+    
+    def __init__(self,filepath,format="podcast"):
 
-    with open(filepath) as f:
-        file = f.read()
+        self.filepath = filepath
+        self.format = format
 
-        html = markdown.markdown(file)
+        self.paragraphs = []
+        self.doc_results = dict()
+        self.doc_cost = 0
+        self.saved = False
 
-        soup = BeautifulSoup(html)
-        document = []
-        ## specific to podcast format
-        for para in soup.find_all("p")[1:]:
-            document.append(para.get_text())
-    print("Loaded Document")
-    print(f"Estimated cost: {(sum([len(para) for para in document])/4000)* len(prompts)*DAVANCI_COST}")
+        with open(filepath) as f:
+            file = f.read()
 
-    ## Running results
-    results = []
-    print("Api called")
-    for para in tqdm(document):
-        for prompt, model in prompts:
-            results.append(openai.Completion.create(
-            model=model,
-            prompt=prompt+para,
+            html = markdown.markdown(file)
+            soup = BeautifulSoup(html)
+            
+            ## specific to podcast format
+            if self.format == "podcast":
+                for para in soup.find_all("p")[1:]:
+                    self.paragraphs.append(dict(text = para.get_text(),total_cost=0))
+            else:
+                raise Exception(f"{self.format} is not supported")
+            print("Loaded Document")
+    
+    def estimated_cost(self,prompts):
+        """Given prompts run on text takes up most of the cost, given an estimate off the cost"""
+
+        tmp = []
+        for para in self.paragraphs:
+            for prompt in prompts:
+                if prompt["on"] == "text":
+                    tmp.append(len(para["text"])*MODEL_COST[prompt['model']])
+        
+        return f"Estimated cost: {sum(tmp)/4000}"
+
+
+    def total_cost(self):
+
+        return sum([para["total_cost"] for para in self.paragraphs]) + self.doc_cost
+
+    
+    @staticmethod
+    def run_open_ai(text,prompt):
+         ## Running results
+
+        ## Limit prompt length
+        combined_prompt = (prompt["full"]+text)[:min(3750*4,len(prompt["full"]+text))]
+        result = openai.Completion.create(
+            model=prompt["model"],
+            prompt=combined_prompt,
             temperature=0.5,
             max_tokens=500,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
-            ))
-    print("GPT3 Run")
+            )
 
-    print(f"Actual cost: {(sum([result['usage']['total_tokens'] for result in results])/1000) * 0.0200}")
+        ## Removes 2 new lines usually there between query and result
+        text = re.sub("^\n\n","",result['choices'][0]['text'])
+        cost = (result["usage"]["total_tokens"]/1000)*MODEL_COST[prompt["model"]]
 
-    ## Clean up results and make list into 2D array grouped by paragraph
-    results_flat = np.reshape(results,-1)
-    results_text = [result['choices'][0]['text'] for result in results_flat]
-
-    results_text_clean = [re.sub("^\n\n","",result) for result in results_text]
-    results_grouped = np.reshape(results_text_clean,(-1,len(prompts)))
-
-    print("Results Cleaned")
+        return text,cost
 
 
-    
-    ## Save to .md file
-    alias_break_count = 0
-    result_i =0
-    with open(filepath) as f_old, open(filepath[:-3] + " + GPT3.md", "w") as f_new:
-        for line in f_old:
 
-            if 'Transcript:' in line:
-                f_new.write(f"GPT-3 Summary:\n")
-                ## Tech debt - prompt titles are not coupled to prompt results
-                f_new.write(f"#### Themes:\n{results_grouped[result_i][0]}\n")
-                f_new.write(f"#### Key points:\n{results_grouped[result_i][1]}\n")
-                f_new.write(f"#### Notes:\n\n")
-                f_new.write("##### ")
-                result_i +=1
-            elif "---" in line:
-                if alias_break_count>1:
-                    f_new.write("####\n")
-                else:
-                    alias_break_count += 1  
-            elif "Status::" in line:
-                line = "- Status:: #📥/🟧 "
-            f_new.write(line)
 
-    print("Saved to file")
 
-    return results_grouped
-    
+    def process(self,prompts = [
 
-        
+        {"short": "Main themes",
+                        "full":"Summarise into top themes in format \n'- [[<theme>]]\n' for the following text:\n\n",
+                        "model": "text-davinci-002",
+                        "on":"Key Points"},
+        {"short": "Summary",
+                        "full":"Summarise main concepts within a short concise paragraph the following text:\n\n",
+                        "model": "text-davinci-002",
+                        "on":"Key Points"
+                        },         
+        {"short": "Key Points",
+            "full":"Summarise into key points in format \n'- point\n' for the following text:\n\n",
+            "model": "text-davinci-002",
+                  "on":"text"}]
+                  ):
+
+        # {"short":"Themes",
+        #     "full": "Capture top 3 themes in format \n - 'theme'\n for text:\n\n",
+        #     "model": "text-davinci-002",
+        #         "on":"text"}
+
+        print(self.estimated_cost(prompts))
+
+        self.prompts = prompts
+
+
+        for prompt in prompts:
+
+            if prompt["on"] == "text":
+                for para in tqdm(self.paragraphs):
+                    text, cost = Document.run_open_ai(para['text'],prompt)
+                    para[prompt["short"]] = text
+                    para['total_cost'] += cost
+            
+        ## Document level/summary results - done as a separate loop to prevent summary prompt breaking if text not available
+
+
+        for prompt in tqdm(prompts):
+            if prompt["on"] != "text":
+
+                segments = []
+                include_segments = []
+
+                for para in self.paragraphs:
+                    segments.extend(para[prompt["on"]].split("\n"))
+
+                for segment in segments:
+                     # remove segmments that are over 200 in length to ensure tokens don't blow out
+                    if len(segment) < 200:
+                        include_segments.append(segment)
+                #sample 75 key points as this will only use 75*(200/4) = 3750, currently sample is just random but might need to be stratified across paragraph to ensure coverage  
+                sampled_points = np.random.choice(include_segments,min(75,len(include_segments)))
+
+                text, cost = Document.run_open_ai(" ".join([para[prompt["on"]] for para in sampled_points]),prompt)
+                self.doc_results[prompt['short']] = text
+                self.doc_cost += cost
+
+
+        print(self.total_cost())
+
+    def save(self):
+
+            alias_break_count = 0
+            result_i =0
+            with open(self.filepath) as f_old, open(self.filepath[:-3] + " + GPT3.md", "w") as f_new:
+                for line in f_old:
+                    
+                    if "Status::" in line:
+                        line = "- Status:: #📥/🟧\n"
+
+                    elif "Highlights" in line:
+
+                        f_new.write(f"### GPT-3 Summary:\n")
+
+                        ### Document level results
+
+                        for heading in self.doc_results:
+                            f_new.write(f"#### {heading}:\n{self.doc_results[heading]}\n")
+
+                        
+                    elif 'Transcript:' in line:
+
+                        for prompt in self.prompts:
+
+                            if prompt["on"] == "text":
+                                f_new.write(f"#### {prompt['short']}:\n{self.paragraphs[result_i][prompt['short']]}\n")
+
+                        f_new.write(f"#### Notes:\n\n")
+                        f_new.write("##### ")
+                        result_i +=1
+                    elif "---" in line:
+                        if alias_break_count>1:
+                            f_new.write("####\n")
+                        else:
+                            alias_break_count += 1  
+                    
+                    f_new.write(line)
+
+                self.saved = True
+
+    def overwrite(self):
+
+        if self.saved:
+            os.remove(self.filepath)
+
+            os.rename(self.filepath[:-3] + " + GPT3.md",self.filepath)
+        else:
+            print("Nothing to save")
+
+# %% [markdown]
+# # Ideas
+# - Create PodcastDocument(Document) class structure
+#     - Create an abstract class document that has abstract classes
+#         - load,
+#         - enrich
+#         - save
+#     - These would then be defined within each inherited class
+#     - This would allow the loading of different documents and running compute on them
+
+# %%
+
+
+filepath = f"/Users/mboyens/Documents/SecondBrain/Readwise/Podcasts/309 – John Carmack —  Doom, Quake, VR, AGI, Programming, Video Games, and Rockets.md"
+filepath
+
+doc = Document(filepath)
+
+doc.process()
+
+# %%
+## Analysis on Segment Lengths for Key Points
+import pandas as pd
+
+# %%
+points = []
+for para in doc.paragraphs].split("\n"):
+    points.extend(para["Key Points"].split("\n"))
+
+df = pd.DataFrame(points)
+df.sort_values(['len'])
+
+# %%
+df[df['len'] <200].len.plot(kind='bar')
+
+# %%
+df[df.point.str.len()>200].point.str.len().describe()
+
+# %%
+len(points)
+
+# %%
+os.remove(filepath)
+
+os.rename(filepath[:-3] + " + GPT3.md",filepath)
+
